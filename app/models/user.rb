3511300a3609ebcd1ecb56915f75ec0ced389fa3
Pulkit Wadhwa <pulkit.wadhwa@infibeam.net>
2015-12-03 13:37:12 +0530

    User: Added email as attr_cacheable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   include CacheableModule
-  attr_cacheable :id
+  validates :email, presence: true, uniqueness: true
+  attr_cacheable :id, :email
   has_many :bookings, dependent: :destroy
   cacheable_has_many :bookings
 end
